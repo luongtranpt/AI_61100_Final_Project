5d9132bcbe0852fb780b2324ea1ab982b44428b4
luongtv <luongtran56789@gmail.com>
2026-06-13 13:23:32 +0900

    ts-cts-no-vel: CTS training without velocity in teacher encoder, rough terrain mix

--- src/tron1_rl_mjlab/__init__.py
+++ src/tron1_rl_mjlab/__init__.py
@@ -2,7 +2,6 @@ from mjlab.tasks.registry import register_mjlab_task
 
 from .tasks.cfg.wf_tron_env_cfg import (
     make_wf_tron_env_cfg,
-    make_wf_tron_flat_env_cfg,
     make_wf_tron_play_env_cfg,
 )
 from .tasks.cfg.wf_tron_rl_cfg import make_wf_tron_rl_cfg
@@ -10,7 +9,7 @@ from .tasks.runner import WFTronPhaseRunner
 
 register_mjlab_task(
     task_id="Mjlab-WF-Tron",
-    env_cfg=make_wf_tron_flat_env_cfg(),   # phase 1a: flat terrain
+    env_cfg=make_wf_tron_env_cfg(),
     play_env_cfg=make_wf_tron_play_env_cfg(),
     rl_cfg=make_wf_tron_rl_cfg(),
     runner_cls=WFTronPhaseRunner,
